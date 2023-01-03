22251f748112175eb3372e21ba8fe356157c4bff
razonyang <razonyang@users.noreply.github.com>
2023-01-03 07:22:41 +0000

    deploy: 48de21d4f818138ce2120752706d2ef84f74c47e

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 (() => {
+  // <stdin>
+  document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector(".theme-toggle").addEventListener("click", () => {
+      if (document.documentElement.hasAttribute("data-theme")) {
+        document.documentElement.removeAttribute("data-theme");
+      } else {
+        document.documentElement.setAttribute("data-theme", "dark");
+      }
+    });
+  });
 })();
 
 ;
