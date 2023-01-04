ce9acfc2d57c59610f507fd9de09e04c56849a69
razonyang <razonyang@users.noreply.github.com>
2023-01-04 12:48:59 +0000

    deploy: 7b85307e206d5acb59d9643d31afb65600983eed

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
 (() => {
   // <stdin>
-  document.addEventListener("DOMContentLoaded", () => {
-    document.querySelector(".theme-toggle").addEventListener("click", () => {
-      if (document.documentElement.hasAttribute("data-theme")) {
-        document.documentElement.removeAttribute("data-theme");
-      } else {
-        document.documentElement.setAttribute("data-theme", "dark");
-      }
+  (() => {
+    document.addEventListener("DOMContentLoaded", () => {
+      document.querySelector(".theme-toggle").addEventListener("click", () => {
+        if (document.documentElement.hasAttribute("data-bs-theme")) {
+          document.documentElement.removeAttribute("data-bs-theme");
+          localStorage.removeItem("theme");
+        } else {
+          document.documentElement.setAttribute("data-bs-theme", "dark");
+          localStorage.setItem("theme", "dark");
+        }
+      });
     });
-  });
+  })();
 })();
 
 ;
