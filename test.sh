2c4bf092b7965ae4f77ce378cb1fb483d8fb8944
razonyang <razonyang@gmail.com>
2023-01-09 15:21:05 +0800

    Update script

--- test.sh
+++ test.sh
@@ -4,7 +4,6 @@ content=`cat ./README.md`
 mkdir -p exampleSite/content/tests
 
 count=100
-echo $count
 if [ ! -z "$1" ]
   then count=$1
 fi
@@ -12,14 +11,18 @@ fi
 for i in $( seq 1 $count ); do
     for j in en zh-hans zh-hant ar; do
         date=`date -d "$i day ago" '+%Y-%m-%d'`
-        echo -e "---\n
-title: '[$j] Test Post #$i'\n
-date: $date\n
-tags:\n
-  - 'Tag #$i'\n
-Categories:\n
-  - Test\n
----\n
-$content" > "exampleSite/content/tests/$i.$j.md"
+        data=`cat <<- data
+---
+title: '[$j] Test Post #$i'
+date: $date
+tags:
+  - 'Tag #$i'
+Categories:
+  - Test
+---
+
+$content
+data`
+        echo "$data" > "exampleSite/content/tests/$i.$j.md"
     done 
 done
