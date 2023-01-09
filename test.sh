8f54f9020f4285daf7019dfa86f0da350c5c320a
razonyang <razonyang@gmail.com>
2023-01-09 15:11:51 +0800

    Fixed Netlify build

--- test.sh
+++ test.sh
@@ -12,14 +12,14 @@ fi
 for i in $( seq 1 $count ); do
     for j in en zh-hans zh-hant ar; do
         date=`date -d "$i day ago" '+%Y-%m-%d'`
-        echo -e "---
-title: '[$j] Test Post #$i'
-date: $date
-tags:
-  - 'Tag #$i'
-Categories:
-  - Test
----
+        echo -e "---\n
+title: '[$j] Test Post #$i'\n
+date: $date\n
+tags:\n
+  - 'Tag #$i'\n
+Categories:\n
+  - Test\n
+---\n
 $content" > "exampleSite/content/tests/$i.$j.md"
     done 
 done
