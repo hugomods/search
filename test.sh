11597777d74ef0858e217de17accdb9eb6c0c6e3
razonyang <razonyang@gmail.com>
2023-01-09 14:53:32 +0800

    Add a script to create test posts

---
+++ test.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+content=`cat ./README.md`
+
+mkdir -p exampleSite/content/tests
+
+count=100
+echo $count
+if [ ! -z "$1" ]
+  then count=$1
+fi
+
+for i in $( seq 1 $count ); do
+    for j in en zh-hans zh-hant ar; do
+        date=`date -d "$i day ago" '+%Y-%m-%d'`
+        echo -e "---
+title: '[$j] Test Post #$i'
+date: $date
+tags:
+  - 'Tag #$i'
+Categories:
+  - Test
+---
+$content" > "exampleSite/content/tests/$i.$j.md"
+    done 
+done
